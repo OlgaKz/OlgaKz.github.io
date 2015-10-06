8cbee24a04e7cf292202dc2d643583be401cdc61
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-10-06 18:39:49 +0300

    h/w  1-2 js is done

--- javascript/homework/lessons_1/task1.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by sone4ko on 06.10.15.
- */
-var developer,name;
-name='Sergey';
-developer='Sergey';
-alert(developer);
--- javascript/homework/lessons_1/task2.js
+++
@@ -1,6 +0,0 @@
-var username;
-username=prompt('What is your name?');
-alert(username);
-console.log(username);
-username=confirm('It is really your name?');
-console.log(username);
