8fca0366622b503185c96e9e995c52a552b4d078
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-10-14 22:58:41 +0300

    js 34l-1 is done

---
+++ javascript/homework/lessons4/task1.js
@@ -0,0 +1,12 @@
+function fib(n){
+    if(n==0){
+        return n;
+    }
+    if(n==1){
+        return n;
+    }
+    if(n>1) {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+console.log(fib(7));
