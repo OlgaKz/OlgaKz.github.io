c68a6f5a6a143c4c449fbbc61b889c74860638cd
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-10-17 22:37:36 +0300

    js l5-1, 2 is done

---
+++ javascript/homework/lessons_5/task2.js
@@ -0,0 +1,19 @@
+var image = {
+    width: 100,
+    height: 400,
+    title: 'Cool image'
+};
+
+
+function multiplyNumeric(myObj) {
+    for (item in myObj) {
+        if ( (typeof(myObj[item]) != 'string') && (typeof (myObj[item]) == 'number')) {
+            myObj[item] = myObj[item] * 2;
+        }
+    }
+    return myObj;
+}
+
+
+
+console.log(multiplyNumeric(image));
