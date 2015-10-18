47ecd8700251c224de7a59a04b00977dea767f47
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-10-18 22:52:12 +0300

    js l5-1, 2, 3  is done

--- javascript/homework/lessons_5/task2.js
+++ javascript/homework/lessons_5/task2.js
@@ -7,13 +7,12 @@ var image = {
 
 function multiplyNumeric(myObj) {
     for (item in myObj) {
-        if ( (typeof(myObj[item]) != 'string') && (typeof (myObj[item]) == 'number')) {
+        if ((typeof(myObj[item]) != 'string') && (typeof (myObj[item]) == 'number')) {
             myObj[item] = myObj[item] * 2;
         }
     }
     return myObj;
 }
-
 
 
 console.log(multiplyNumeric(image));
