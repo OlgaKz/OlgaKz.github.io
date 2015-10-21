3c4c4db6010d587d3f8cd9dc7255be14a4a7a165
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-10-21 13:47:58 +0300

    js l6-6,7 is done

---
+++ javascript/homework/lessons_6/task7.js
@@ -0,0 +1,16 @@
+function anClean(arr){
+    var obj = {};
+    for(var i = 0; i <arr.length; i++){
+        var sorted = arr[i].toLowerCase().split('').sort().join('');
+        obj[sorted] = arr [i];
+    }
+    console.log(obj) ;
+    var result = [];
+    for(var key in obj) {
+        result.push(obj[key]);
+    }
+    return result;
+}
+var arr = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
+
+console.log(anClean(arr));
