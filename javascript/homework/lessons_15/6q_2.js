643ff17291781f99bde8db5af3d2698c759e22a0
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-12-09 14:09:43 +0200

    l 15 6Q is done

--- javascript/homework/lessons_15/6q_2.js
+++ javascript/homework/lessons_15/6q_2.js
@@ -1,5 +1,13 @@
 Object.prototype.hash = function(string) {
-    return this.string;
+    var obj = this;
+    string = string.split('.');
+    for(var i =0; i< string.length; i++){
+        if(!obj){
+            return;
+        }
+        obj = obj[string[i]];
+    }
+    return obj;
 };
 
 var obj = {
--- javascript/homework/lessons_15/6q_3.js
+++ javascript/homework/lessons_15/6q_3.js
@@ -1,5 +1,10 @@
-
 function construct(Class) {
-    var fun = Class.bind.apply(Class, arguments);
-    return new fun();
+    var args = Array.prototype.slice.call(arguments, 1);
+    var object = Object.create(Class.prototype);
+    var result = Class.apply(object, args);
+    if (typeof result === 'object') {
+        return result;
+    } else {
+    return object;
+    }
 }
