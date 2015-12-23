927a549165c02044249936c922cd7230e4b43f5a
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-12-23 17:46:52 +0200

    pr MC edit 2 doctors

--- project_site/js/doctors.js
+++ project_site/js/doctors.js
@@ -1,6 +1,22 @@
 var popup = document.body.getElementsByClassName('doc-name');
-popup.addEventListener('click',showPopup);
-function showPopup(){
-    document.body.getElementsByClassName('popup-doc')[0].style.display = 'block';
+    popup[0].addEventListener('click', showPopup);
+
+var buttonClose = document.body.getElementsByClassName('close');
+    buttonClose[0].addEventListener('click', closePopup);
+function closePopup(){
+        document.body.getElementsByClassName('popup-doc')[0].style.display = 'none';
 }
+function showPopup(){
+        document.body.getElementsByClassName('popup-doc')[0].style.display = 'block';
+}
+popup.onclick = function(event){
+    var target = event.target;
+    if(target.className!= 'popup-doc')return;
+    showPopup(target);
+};
+buttonClose.onclick = function(event){
+    var targetClose = event.target;
+    if(targetClose.className!= 'close')return;
+    closePopup(targetClose);
+};
 
