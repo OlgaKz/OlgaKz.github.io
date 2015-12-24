5a358807e4542b97b2bb46a7c778e009022efde7
OlgaKz <olga.kuznetsovasp3@gmail.com>
2015-12-24 15:43:24 +0200

    pr MC add jquery to doctors

--- project_site/js/doctors.js
+++ project_site/js/doctors.js
@@ -1,22 +1,14 @@
-var popup = document.body.getElementsByClassName('doc-name');
-    popup[0].addEventListener('click', showPopup);
-
-var buttonClose = document.body.getElementsByClassName('close');
-    buttonClose[0].addEventListener('click', closePopup);
-function closePopup(){
-        document.body.getElementsByClassName('popup-doc')[0].style.display = 'none';
-}
-function showPopup(){
-        document.body.getElementsByClassName('popup-doc')[0].style.display = 'block';
-}
-popup.onclick = function(event){
-    var target = event.target;
-    if(target.className!= 'popup-doc')return;
-    showPopup(target);
-};
-buttonClose.onclick = function(event){
-    var targetClose = event.target;
-    if(targetClose.className!= 'close')return;
-    closePopup(targetClose);
-};
-
+$(function () {
+    //script for popups
+    $('a.show_popup').click(function () {
+        $('div.' + $(this).attr("rel")).fadeIn(500);
+        $("body").append("<div id='overlay'></div>");
+        $('#overlay').show().css({'filter': 'alpha(opacity=80)'});
+        return false;
+    });
+    $('button.close').click(function () {
+        $(this).parent().fadeOut(100);
+        $('#overlay').remove('#overlay');
+        return false;
+    });
+});
