13a6090160f70cbc8d3ce540cb7ae3eeda7e571d
OlgaKz <olga.kuznetsovasp3@gmail.com>
2016-01-03 16:43:08 +0200

    pr MC remake galery

--- project_site/js/galery.js
+++ project_site/js/galery.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $('.zoom-galery').hide();
-    $('.zoom').click(function(e) {
-        $(e.target).parent().children('.zoom-galery').show();
-    })
+$(".item-galery").on("click", ".zoom", function (event) {
+
+    //let's get parent and then it's child with certain class name
+    var parentElem = $(this).parent();
+    var childImageElem = parentElem.find(".images");
+    //let's prepare our zoom-image block
+    $(".zoom-image").attr("src", childImageElem.attr('src'));
+    $(".zoom-galery").css("display", "inline");
+
+    // add wrapper
+    $("body").append("<div id='overlay'></div>");
+    $('#overlay').show().css({'filter': 'alpha(opacity=80)'});
+    return false;
 });
